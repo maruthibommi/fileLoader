5f0a4ed6522d1edc2639aacf029f71b06a1804ad
Yagna <yagnaveeranarayan@gmail.com>
2022-08-30 16:40:02 +0530

    changes

--- fileLoad/urls.py
+++ fileLoad/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from fileLoad import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.hello_there, name="home"),
 ]
--- fileLoad/views.py
+++ fileLoad/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
     return HttpResponse("Hello, Django!")
+
+def hello_there(request):
+    return render(request,'fileLoad/Html_css_files/mainLoad.html')
